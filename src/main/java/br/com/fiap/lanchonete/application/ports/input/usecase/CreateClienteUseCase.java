5fc92ddb19bcaa59d238c014dcffe94b09d8bcac
Adriano Pavao <adribenig@hotmail.com>
2023-10-21 10:08:01 -0300

    ajuste ports

--- src/main/java/br/com/fiap/lanchonete/application/ports/input/usecase/CreateClienteUseCase.java
+++ src/main/java/br/com/fiap/lanchonete/application/ports/input/usecase/CreateClienteUseCase.java
@@ -1,7 +1,6 @@
 package br.com.fiap.lanchonete.application.ports.input.usecase;
 
 import br.com.fiap.lanchonete.adapters.driver.dto.ClienteDTO;
-import org.springframework.stereotype.Component;
 
 public interface CreateClienteUseCase {
 
--- src/main/java/br/com/fiap/lanchonete/application/ports/input/usecase/GetClienteUseCase.java
+++ src/main/java/br/com/fiap/lanchonete/application/ports/input/usecase/GetClienteUseCase.java
@@ -1,9 +1,8 @@
 package br.com.fiap.lanchonete.application.ports.input.usecase;
 
 import br.com.fiap.lanchonete.adapters.driver.dto.ClienteDTO;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public interface GetClienteUseCase {
 
     ClienteDTO retrieveCliente(String cpf);
