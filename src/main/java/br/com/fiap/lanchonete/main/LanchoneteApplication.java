e2afc0fda75df312bef464a2149d364bf1d5860e
Gian Gomes <g_gomees@hotmail.com>
2024-01-26 18:17:18 -0300

    Mover LanchoneteApplication pro package main

--- src/main/java/br/com/fiap/lanchonete/main/LanchoneteApplication.java
+++ src/main/java/br/com/fiap/lanchonete/main/LanchoneteApplication.java
@@ -1,4 +1,4 @@
-package br.com.fiap.lanchonete;
+package br.com.fiap.lanchonete.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
